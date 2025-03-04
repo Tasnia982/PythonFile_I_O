4abb4e99b80afa429f54ecf7b162f68c15477307
Tasnia982 <rahman35-982@diu.edu.bd>
2025-03-05 00:09:40 +0600

    Try Except practice

---
+++ 6TryExcept.py
@@ -0,0 +1,20 @@
+a=1
+b=0
+try:
+    print(a/b)
+
+except ZeroDivisionError:
+    print("Error is ")
+
+
+c=int(input("Please enter a number : "))
+d=int(input("Please enter another number : "))
+try:
+    print(c/d)
+    print(c+d)
+except :
+    print("Error is ")
+
+print("Program is ended ")
+    
+
